69d2ce815d8f650280f8c9170f4157296e925c6c
kamlendr <kamlendra92@gmail.com>
2021-06-08 20:24:07 +0530

    JioMart

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Switch, Route, Redirect,Link } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return <div className='App'>JIOMART</div>;
+}
+
+export default App;
---
+++ src/Store/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({});
+
+const enhancer = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+  export const store = createStore(rootReducer, enhancer);
+  

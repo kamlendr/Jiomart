c2590d25c6f03b0ffc44aae519129ce05ae1120f
Raushan8271 <raushan8271@gmail.com>
2021-06-09 16:36:55 +0530

    not done

--- src/Components/landingPage/LandingPage.jsx
+++ src/Components/landingPage/LandingPage.jsx
@@ -5,10 +5,8 @@ function LandingPage() {
     return (
         <>
             <div className={style.LandingPage}>
-
                 <button className={style.sliderButton}><img className={style.buttonImg} src="https://www.jiomart.com/assets/version1620668133/smartweb/images/icons/banner_arrow_left.png" alt="" /></button>
                 <button className={style.sliderButton}><img className={style.buttonImg} src="https://www.jiomart.com/assets/version1620668133/smartweb/images/icons/banner_arrow_right.png" alt="" /></button>
-
             </div>
 
             {/* <div style={{ background: "black", width: "100%", height: "800px" }}>

30ce51627b65a6ed54355c617982ac18e350d5de
Raushan8271 <raushan8271@gmail.com>
2021-06-09 16:34:17 +0530

    not done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Switch, Route, Redirect,Link } from 'react-router-dom';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
 import './App.css';
+import LandingPage from './Components/landingPage/LandingPage';
 import { NavBar } from './Components/Navbar/Navbar';
 
 function App() {
   return <div className='App'>
-      <NavBar/>
+    <NavBar />
+    <LandingPage />
 
   </div>;
 }
--- src/Components/landingPage/LandingPage.jsx
+++ src/Components/landingPage/LandingPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import style from './LandingPage.module.css'
 
 function LandingPage() {
     return (
-        <div>
-            <h1>Landing</h1>
-        </div>
+        <>
+            <div className={style.LandingPage}>
+
+                <button className={style.sliderButton}><img className={style.buttonImg} src="https://www.jiomart.com/assets/version1620668133/smartweb/images/icons/banner_arrow_left.png" alt="" /></button>
+                <button className={style.sliderButton}><img className={style.buttonImg} src="https://www.jiomart.com/assets/version1620668133/smartweb/images/icons/banner_arrow_right.png" alt="" /></button>
+
+            </div>
+
+            {/* <div style={{ background: "black", width: "100%", height: "800px" }}>
+
+            </div> */}
+        </>
     )
 }
 
